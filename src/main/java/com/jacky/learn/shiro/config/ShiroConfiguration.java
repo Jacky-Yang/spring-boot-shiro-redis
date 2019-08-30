1287cece41bfcc753a5053ee209c0d9c8164b1b7
yangjie <yangjie3232006@126.com>
2019-08-30 18:31:25 +0800

    配置安全管理器

--- src/main/java/com/jacky/learn/shiro/config/ShiroConfiguration.java
+++ src/main/java/com/jacky/learn/shiro/config/ShiroConfiguration.java
@@ -2,48 +2,58 @@ package com.jacky.learn.shiro.config;
 
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
-import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ShiroConfiguration {
 
-//    private static final String loginUrl = "login.html";
-//
-//    @Autowired
-//    protected SecurityManager securityManager;
-//
-//    @Autowired
-//    protected ShiroFilterChainDefinition shiroFilterChainDefinition;
-//
-//    @Bean
-//    protected ShiroFilterFactoryBean shiroFilterFactoryBean() {
-//        ShiroFilterFactoryBean filterFactoryBean = new ShiroFilterFactoryBean();
-//
-//        filterFactoryBean.setLoginUrl(loginUrl);
-////        filterFactoryBean.setSuccessUrl(successUrl);
-////        filterFactoryBean.setUnauthorizedUrl(unauthorizedUrl);
-////
-//        filterFactoryBean.setSecurityManager(securityManager);
-//        filterFactoryBean.setFilterChainDefinitionMap(shiroFilterChainDefinition.getFilterChainMap());
-//
-//        return filterFactoryBean;
-//    }
+    private ShiroProperties shiroProperties;
 
-    @Bean
-    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
-        DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
-        chainDefinition.addPathDefinition("/login.html", "anon");
-        chainDefinition.addPathDefinition("/**", "authc");
-        return chainDefinition;
+    private ShiroService shiroService;
+
+    public ShiroConfiguration(ShiroProperties shiroProperties, ShiroService shiroService) {
+        this.shiroProperties = shiroProperties;
+        this.shiroService = shiroService;
     }
 
     @Bean
-    public Realm authorizingRealm() {
+    protected ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean filterFactoryBean = new ShiroFilterFactoryBean();
+
+        filterFactoryBean.setSecurityManager(securityManager);
+        filterFactoryBean.setFilterChainDefinitionMap(shiroService.loadFilterChainDefinitions());
+
+        return filterFactoryBean;
+    }
+
+    /**
+     * 配置 SecurityManager Bean
+     * SecurityManager 继承了SessionManager接口，但是具体实现委托给了具体的SessionManager的实现类
+     * @return SecurityManager
+     */
+    @Bean
+    public SecurityManager securityManager() {
+        // 由于是Web项目，所以使用Web安全管理器，
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(authorizingRealm());
+
+        securityManager.setSessionManager(sessionManager());
+        return  securityManager;
+    }
+
+    private Realm authorizingRealm() {
+        System.out.println("authorizingRealm");
         return new UserRealm();
+    }
+
+    private SessionManager sessionManager() {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+
+        return sessionManager;
     }
 }
